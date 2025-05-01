5b33e30ac322f11d21b96135dc5b56b418752f2f
ubash <subashs20018@gmail.com>
2025-05-01 04:00:27 +0000

    connection timeout errror has been rectified and set to 5 minutes and add error handling for mmua feedback loop

--- gui.py
+++ gui.py
@@ -5,15 +5,17 @@ from gui.callbacks import GUIState
 import gui.error_pages
 
 
-icon_image_b64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAAAACXBIWXMAAAsSAAALEgHS3X78AAAWd0lEQVR4nO2dfYxc1XnGz+587+x49tO7ttf2GK/tpTZ4mlwawF9rbPMRzIeAEGTTUgpSCzSp1EopSImEaNVUVUWTqFFVCQilIk2bokYptKEJASJopXaimoAEIaYYB7telvXudnZ2d762qsd7R6x3Z9c765lz3rnn+Un+gz/Y+5477/vcc9773HOaSqWSIoTYSTN/d0LshQJAiMVQAAixGAoAIRZDASDEYigAhFgMBYAQi6EAEGIxFABCLIYCQIjFUAAIsRgKACEWQwEgxGIoAIRYDAWAEIuhABBiMRQAQiyGAkCIxVAACLEYCgAhFkMBIMRiKACEWAwFgBCLoQAQYjEUAEJsRSn1/wo3KFPhDTaqAAAAAElFTkSuQmCC'
+icon_image_b64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAAAACXBIWXMAAAsSAAALEgHS3X78AAAWd0lEQVR4nO2dfYxc1XnGz+587+x49tO7ttf2GK/tpTZ4mlwawF9rbPMRzIeAEGTTUgpSCzSp1EopSImEaNVUVUWTqFFVCQilIk2bokYptKEJASJopXaimoAEIaYYB7telvXudnZ2d762qsd7R6x3Z9c765lz3rnn+Un+gz/Y+5577/vcc9773HOaSqWSIoTYSTN/d0LshQJAiMVQAAixGAoAIRZDASDEYigAhFgMBYAQi6EAEGIxFABCLIYCQIjFUAAIsRgKACEWQwEgxGIoAIRYDAWAEIuhABBiMRQAQiyGAkCIxVAACLEYCgAhFkMBIMRiKACEWAwFgBCLoQAQYjEUAEJsRSn1/wo3KFPhDTaqAAAAAElFTkSuQmCC'
 
 @ui.page('/')
 async def index(client: Client):
     # Start the interface!
     gui_st = GUIState()
-
-    # Connection end
-    # https://github.com/zauberzeug/nicegui/discussions/1379
+    
+    # Wait for client to disconnect before cleaning up
+    await client.disconnected()
+    
+    # Connection end - only execute this when client actually disconnects
     print('Closed connection ', gui_st.pattern_state.id, '. Deleting files...')
     gui_st.release()
 
@@ -23,6 +25,6 @@ if __name__ == '__main__':
             favicon=icon_image_b64,
             title='GarmentCode',
             show=False,
-            # reconnect_timeout=90
+            reconnect_timeout=300  # Increase reconnect timeout to 2 minutes
         )
     
